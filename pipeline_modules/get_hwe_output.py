878bee42a217bfb95b898a798a6abd349a8a3558
Raya Esplin <Raya Esplin>
2024-02-19 22:08:33 +0000

    created a reduced vcf that only keeps hops in hwe using vcftools

--- pipeline_modules/get_hwe_output.py
+++ pipeline_modules/get_hwe_output.py
@@ -10,3 +10,7 @@ vcf_calc.run(vcf="../hops.vcf", calc_statistic="hardy-weinberg", out=OUTPUT_PREF
 if not os.path.exists(output_dir_path): os.mkdir(output_dir_path)
 
 os.system(f"mv {OUTPUT_PREFIX}* {output_dir_path}")
+
+# create a “reduced” file with only loci that are in HWE at a p-value cutoff of 0.05
+os.system(f"vcftools --vcf ../hops.vcf --hwe 0.05 --recode --stdout > {output_dir_path}/hops_in_hwe.vcf")
+
