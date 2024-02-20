26e372caf9861b71c630ad613157875572ea0362
Raya Esplin <Raya Esplin>
2024-02-20 00:09:54 +0000

    adds functionality to find the best fit k value

--- pipeline_modules/estimate_population_structure.py
+++ pipeline_modules/estimate_population_structure.py
@@ -3,22 +3,46 @@ import os
 plink_dir_path = f"output/plink"
 admixture_dir_path = f"output/admixture"
 
+
 def make_output_dirs():
     # make output dir for plink
-    if not os.path.exists(plink_dir_path): os.mkdir(plink_dir_path)
+    if not os.path.exists(plink_dir_path):
+        os.mkdir(plink_dir_path)
     # make otuput dir for admixture
-    if not os.path.exists(admixture_dir_path): os.mkdir(admixture_dir_path)
+    if not os.path.exists(admixture_dir_path):
+        os.mkdir(admixture_dir_path)
 
 
-def run_admixture(vcf_file, num_of_subpops_to_test=5, output_prefix=""):
+def run_admixture(num_subpops_to_test, output_prefix):
+    # run admixture to test different number of subpopulations
+    for k in range(0, num_subpops_to_test):
+        os.system(
+            f"admixture --cv output/plink/{output_prefix}.bed {k} > {admixture_dir_path}/log{k}.out"
+        )
+    os.system(f"mv {output_prefix}* output/admixture")
+
+
+def least_cv_error(output_prefix):
+    # estimate which has the least cross validation error
+    os.system(f"bash scripts/bash/estimate_least_cv_error.sh {admixture_dir_path} {output_prefix}")
+    return find_k_least_cv(f"{admixture_dir_path}/{output_prefix}.cv.error")
+    
+
+
+def find_k_least_cv(file_path):
+    with open(file_path) as f:
+        min_row = min(f, key=lambda line: float(line.split()[1]))
+    return min_row.split()[0]
+
+
+def run(vcf_file, num_subpops_to_test=5, output_prefix=""):
     unique_output_prefix = f"{output_prefix}_structure"
     make_output_dirs()
     # convert vcf to bim for admixture
-    os.system(f"bash scripts/bash/convert_vcf_to_bim.sh {vcf_file} {unique_output_prefix}")
+    os.system(
+        f"bash scripts/bash/convert_vcf_to_bim.sh {vcf_file} {unique_output_prefix}"
+    )
 
-    # run admixture to test different number of subpopulations
-    for k in range(0, num_of_subpops_to_test):
-        os.system(f"admixture --cv output/plink/{unique_output_prefix}.bed {k} > {admixture_dir_path}/log{k}.out")
-    os.system(f"mv {unique_output_prefix}* output/admixture")
-    
-    
+    # run_admixture(num_subpops_to_test, unique_output_prefix)
+    best_fit_k = least_cv_error(unique_output_prefix)
+   
