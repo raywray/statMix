67f4aad0843b666fee73e99e2ddc92d97e4f92ab
Raya Esplin <resplin5072@sdsu.edu>
2024-03-18 12:59:18 -0700

    will need to fill out when have a vcf that is not messed up.

--- pipeline_modules/generate_f_stats.py
+++ pipeline_modules/generate_f_stats.py
@@ -3,18 +3,11 @@ import os
 OUTPUT_DIR = "output"
 F_STATS_DIR = f"{OUTPUT_DIR}/f_stats"
 
-def execute_command(command):
-    os.system(command)
+"""
+TODO: fix this when have a vcf that will work to test this
 
-def create_directory(dir_path):
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-
-def convert_bed_to_eigenstrat(bed_file_prefix):
-    """
-    THIS HAS A BUG -- Right now, the error is: 
-    
-    initLogger - WARNING: PPP, version 0.1.12
+Using the hops vcf results in this error: 
+initLogger - WARNING: PPP, version 0.1.12
     exp_handler - ERROR: fatalx:
     bad chrom: 000000
     Traceback (most recent call last):
@@ -30,20 +23,31 @@ def convert_bed_to_eigenstrat(bed_file_prefix):
         raise Exception(convertf_stderr)
     Exception: fatalx:
     bad chrom: 000000
-    """
-    convert_command_list = [
+"""
+
+def execute_command(command_list):
+    command = " ".join(command_list)
+    os.system(command)
+
+def create_directory(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+def convert_bed_to_eigenstrat(bed_file_prefix):
+    convert_command = [
         "vcf_format_conversions.py",
         f"--binary-ped-prefix {bed_file_prefix}",
         f"--out-prefix {F_STATS_DIR}/hops",
         "--out-format eigenstrat"
     ]
-    convert_command = " ".join(convert_command_list)
     execute_command(convert_command)
 
-def calculate_pattersons_d():
-    eigenstrat_prefix = ""
+def calculate_pattersons_d(eigenstrat_prefix):
+    """
+    TODO: finish filling this out
+    """
 
-    pattersons_command_list = [
+    pattersons_command = [
         "eigenstrat_fstats.py",
         f"--eigenstrat-prefix {eigenstrat_prefix}",
         "--calc-admix-statistic D",
@@ -52,8 +56,6 @@ def calculate_pattersons_d():
         "--admix-y-pop",
         "--admix-z-pop"
     ]
-
-    pattersons_command = " ".join(pattersons_command_list)
     execute_command(pattersons_command)
 
     """
@@ -73,7 +75,7 @@ def run(bed_file_prefix):
     create_directory(F_STATS_DIR)
 
     # prepare for eigenstrat
-    # TODO uncomment when this is fixed
-    # convert_bed_to_eigenstrat(bed_file_prefix) 
+    convert_bed_to_eigenstrat(bed_file_prefix) 
 
-run("output/plink/hops_structure")
+    eigenstrat_prefix = bed_file_prefix # TODO change this when you get more information
+    calculate_pattersons_d(eigenstrat_prefix)
