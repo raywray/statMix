511aa9dc4fffd2fc92c96000be8872101637b2d6
Your Name <resplin5072@sdsu.edu>
2024-06-11 15:53:02 -0700

    adds print statements for clarification

--- statmix.py
+++ statmix.py
@@ -23,6 +23,7 @@ def run_summary_stats():
     # perform analyses specified from user
     if "hwe" in args.analyses:
         generate_hwe_results.run(vcf_file=vcf_file, p_value=args.p_val)
+        print("finished hwe")
     
     if "pop_structure" in args.analyses:
         best_fit_k = estimate_population_structure.run(
@@ -30,8 +31,11 @@ def run_summary_stats():
             output_prefix=output_prefix,
             num_subpops_to_test=args.subpops_to_test,
         )
+        print("finished pop_structure")
+    
     if "sfs" in args.analyses:
         generate_sfs.run(vcf_file=vcf_file, k=best_fit_k)
+        print("finished sfs")
     
     if "generic_stats" in args.analyses:
         generate_generic_stats.run(
@@ -40,11 +44,13 @@ def run_summary_stats():
             statistic_window_size=args.statistic_window_size,
             k=best_fit_k,
         )
+        print("finished generic_stats")
     
     if "fsc" in args.analyses:
         generate_sfs_fsc.run(
             vcf_file=vcf_file, k=best_fit_k, output_prefix=output_prefix
         )
+        print("finished fsc")
     
     if "pixy" in args.analyses:
         generate_pixy_stats.run(
@@ -53,14 +59,17 @@ def run_summary_stats():
             window_size=args.statistic_window_size,
             k=best_fit_k,
         )
+        print("finished pixy")
     
     if "ima" in args.analyses:
         run_ima_analysis.run(
             vcf_file=vcf_file, k=best_fit_k, output_prefix=output_prefix
         )
+        print("finished ima")
     
     if "f_stats" in args.analyses:
         generate_f_stats.run(vcf_file=vcf_file, output_prefix=output_prefix)
+        print("finished f_stats")
 
     # print this after all other analyses run
     if "pop_structure" in args.analyses:
